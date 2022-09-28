0cf4cb4fd57ec6eb7c44c075a2cf3d7d9bd30494
Badhon100 <badhonx102@gmail.com>
2022-09-28 15:29:05 +0600

    getting back to nav screen before exiting from hom and friend tab

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return 
     MultiProvider(
-    providers: [
-      
-
-              
+    providers: [         
       ChangeNotifierProvider(create: (_) => LoginProvider()),
       ChangeNotifierProvider(create: (_) => RegistrationProvider()),
       ChangeNotifierProvider(create: (_) => ShowPasswordProvider()),
