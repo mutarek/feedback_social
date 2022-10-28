78a1ac09753dfa12e5530650db169f4a6eeb874c
Mehedi Hasan Shuvo <67133203+duetianmehedishuvo@users.noreply.github.com>
2022-10-28 23:54:02 +0600

    Profile screen Refresh indicator system implemented

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -26,6 +26,11 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   ScrollController controller = ScrollController();
 
+  Future<void> _refresh(BuildContext context) async {
+    Provider.of<ProfileProvider>(context, listen: false).initializeAllUserPostData((bool status) {}, isFirstTime: false);
+    Provider.of<ProfileProvider>(context, listen: false).initializeUserData();
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -50,102 +55,116 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: SafeArea(
         child: Consumer2<AuthProvider, ProfileProvider>(
           builder: (context, authProvider, profileProvider, child) => profileProvider.isLoading || profileProvider.isProfileLoading
-              ?const profilePostShimmerWidget()
-              : SingleChildScrollView(
-                  controller: controller,
-                  physics: const ScrollPhysics(),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Stack(
-                        children: [
-                          Container(height: height * 0.26, width: width, color: Palette.scaffold),
-                          ProfileCoverPhotoWidget(
-                              back: () {
-                                Navigator.of(context).pop();
-                              },
-                              viewCoverPhoto: (() {
-                                Get.to(() => SingleImageView(imageURL: profileProvider.userprofileData.coverImage!));
-                              }),
-                              coverphoto: (profileProvider.isProfileLoading == false)
-                                  ? profileProvider.userprofileData.coverImage!
-                                  : "https://meektecbacekend.s3.amazonaws.com/media/profile/default.jpeg",
-                              coverphotochange: (() {
-                                Navigator.of(context)
-                                    .push(MaterialPageRoute(builder: (_) => const UpdateCoverPhoto(isCoverPhotoUpload: true)));
-                              })),
-                          ProfilePhotowidget(
-                              profilePhotoChange: () {
-                                Navigator.of(context)
-                                    .push(MaterialPageRoute(builder: (_) => const UpdateCoverPhoto(isCoverPhotoUpload: false)));
-                              },
-                              profileImage: (profileProvider.isProfileLoading == false)
-                                  ? profileProvider.userprofileData.profileImage!
-                                  : "https://meektecbacekend.s3.amazonaws.com/media/profile/default.jpeg",
-                              viewProfilePhoto: () {
-                                Get.to(() => SingleImageView(imageURL: profileProvider.userprofileData.profileImage!));
-                              })
-                        ],
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: height * 0.01, left: width * 0.05),
-                        child: Text(
-                          "${profileProvider.userprofileData.firstName!} ${profileProvider.userprofileData.lastName!}",
-                          style: GoogleFonts.lato(fontSize: width * 0.05, fontWeight: FontWeight.w700),
+              ? const profilePostShimmerWidget()
+              : RefreshIndicator(
+                  onRefresh: () {
+                    return _refresh(context);
+                  },
+                  child: SingleChildScrollView(
+                    controller: controller,
+                    physics: const ScrollPhysics(),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Stack(
+                          children: [
+                            Container(height: height * 0.26, width: width, color: Palette.scaffold),
+                            ProfileCoverPhotoWidget(
+                                back: () {
+                                  Navigator.of(context).pop();
+                                },
+                                viewCoverPhoto: (() {
+                                  Get.to(() => SingleImageView(imageURL: profileProvider.userprofileData.coverImage!));
+                                }),
+                                coverphoto: (profileProvider.isProfileLoading == false)
+                                    ? profileProvider.userprofileData.coverImage!
+                                    : "https://meektecbacekend.s3.amazonaws.com/media/profile/default.jpeg",
+                                coverphotochange: (() {
+                                  Navigator.of(context)
+                                      .push(MaterialPageRoute(builder: (_) => const UpdateCoverPhoto(isCoverPhotoUpload: true)));
+                                })),
+                            ProfilePhotowidget(
+                                profilePhotoChange: () {
+                                  Navigator.of(context)
+                                      .push(MaterialPageRoute(builder: (_) => const UpdateCoverPhoto(isCoverPhotoUpload: false)));
+                                },
+                                profileImage: (profileProvider.isProfileLoading == false)
+                                    ? profileProvider.userprofileData.profileImage!
+                                    : "https://meektecbacekend.s3.amazonaws.com/media/profile/default.jpeg",
+                                viewProfilePhoto: () {
+                                  Get.to(() => SingleImageView(imageURL: profileProvider.userprofileData.profileImage!));
+                                })
+                          ],
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: height * 0.01, left: width * 0.04, right: width * 0.04),
-                        child: Container(
-                          height: height * 0.043,
-                          width: width * 0.92,
-                          decoration: const BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(4))),
-                          child: Padding(
-                            padding: EdgeInsets.only(left: width * 0.1),
-                            child: Row(
-                              children: [
-                                InkWell(
-                                  onTap: (){
-                                    Navigator.of(context).push(MaterialPageRoute(builder: (_)=>FriendScreen()));
-                                  },
-                                  child: Row(
-                                    children: [
-                                      Text(
-                                        "${profileProvider.userprofileData.friends!.length}",
-                                        style: GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w500, color: Palette.notificationColor),
-                                      ),
-                                      Text(" Friends", style: GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w500)),
-                                    ],
+                        Padding(
+                          padding: EdgeInsets.only(top: height * 0.01, left: width * 0.05),
+                          child: Text(
+                            "${profileProvider.userprofileData.firstName!} ${profileProvider.userprofileData.lastName!}",
+                            style: GoogleFonts.lato(fontSize: width * 0.05, fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: height * 0.01, left: width * 0.04, right: width * 0.04),
+                          child: Container(
+                            height: height * 0.043,
+                            width: width * 0.92,
+                            decoration: const BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(4))),
+                            child: Padding(
+                              padding: EdgeInsets.only(left: width * 0.1),
+                              child: Row(
+                                children: [
+                                  InkWell(
+                                    onTap: () {
+                                      Navigator.of(context).push(MaterialPageRoute(builder: (_) => FriendScreen()));
+                                    },
+                                    child: Row(
+                                      children: [
+                                        Text(
+                                          "${profileProvider.userprofileData.friends!.length}",
+                                          style:
+                                              GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w500, color: Palette.notificationColor),
+                                        ),
+                                        Text(" Friends", style: GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w500)),
+                                      ],
+                                    ),
                                   ),
-                                ),
-                                SizedBox(width: width * 0.2),
-                                Text(
-                                  "${profileProvider.userprofileData.followers!.length}",
-                                  style: GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w500, color: Palette.notificationColor),
-                                ),
-                                Text(" Followers", style: GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w500))
-                              ],
+                                  SizedBox(width: width * 0.2),
+                                  Text(
+                                    "${profileProvider.userprofileData.followers!.length}",
+                                    style: GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w500, color: Palette.notificationColor),
+                                  ),
+                                  Text(" Followers", style: GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w500))
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: height * 0.01, left: width * 0.04, right: width * 0.04),
-                        child: ProfileDetailsCard(userProfileModel: profileProvider.userprofileData),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: width * 0.02),
-                        child: ListView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: profileProvider.newsFeedLists.length,
-                            itemBuilder: ((context, index) {
-                              return Container(
-                                  margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                                  child: TimeLineWidget(profileProvider.newsFeedLists[index], index, profileProvider, isProfileScreen: true));
-                            })),
-                      )
-                    ],
+                        Padding(
+                          padding: EdgeInsets.only(top: height * 0.01, left: width * 0.04, right: width * 0.04),
+                          child: ProfileDetailsCard(userProfileModel: profileProvider.userprofileData),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: width * 0.02),
+                          child: ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: profileProvider.newsFeedLists.length,
+                              itemBuilder: ((context, index) {
+                                return Container(
+                                    margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                                    child: TimeLineWidget(profileProvider.newsFeedLists[index], index, profileProvider,
+                                        isProfileScreen: true));
+                              })),
+                        ),
+                        profileProvider.isBottomLoading
+                            ? Container(
+                                width: MediaQuery.of(context).size.width,
+                                height: 100,
+                                alignment: Alignment.center,
+                                child: const CupertinoActivityIndicator())
+                            : const SizedBox.shrink()
+                      ],
+                    ),
                   ),
                 ),
         ),
