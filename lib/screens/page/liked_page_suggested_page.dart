5f677b92f9e76058148ec185ba2629e17c8dceba
macbookair <faysaltanvir9991@gmail.com>
2022-11-24 14:19:55 +0600

    my page suggestion page data get and show

--- lib/screens/page/liked_page_suggested_page.dart
+++ lib/screens/page/liked_page_suggested_page.dart
@@ -1,4 +1,7 @@
+import 'package:als_frontend/localization/language_constrants.dart';
 import 'package:als_frontend/provider/page_provider.dart';
+import 'package:als_frontend/util/theme/text.styles.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,6 +19,7 @@ class _likedPageSuggestedPageState extends State<likedPageSuggestedPage> {
     super.initState();
     Provider.of<PageProvider>(context, listen: false).initializeAuthorPageLists();
   }
+
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
@@ -24,187 +28,182 @@ class _likedPageSuggestedPageState extends State<likedPageSuggestedPage> {
       backgroundColor: Colors.white,
       body: Padding(
         padding: EdgeInsets.only(top: height * 0.06, left: 10, right: 10),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back_ios)),
-                SizedBox(
-                  width: width * 0.26,
-                ),
-                const Text(
-                  "Pages",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-            SizedBox(
-              height: height * 0.03,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: height * 0.035,
-                  width: width * 0.24,
-                  decoration: BoxDecoration(
-                      color: const Color(0xff090D2A), borderRadius: BorderRadius.circular(4)),
-                  child: const Center(
-                      child: Text(
-                        "My page",
-                        style: TextStyle(color: Colors.white),
-                      )),
-                ),
-                SizedBox(
-                  width: width * 0.03,
-                ),
-                Container(
-                  height: height * 0.035,
-                  width: width * 0.24,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      color: Colors.white,
-                      border: Border.all(color: Colors.black, width: 1)),
-                  child: const Center(child: Text("Discover")),
-                )
-              ],
-            ),
-            SizedBox(
-              height: height * 0.03,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10, right: 10),
-              child: Row(
-                children: const [
+        child: Consumer<PageProvider>(builder: (context, pageProvider, child) {
+          return Column(
+            children: [
+              Row(
+                children: [
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back_ios)),
+                  SizedBox(
+                    width: width * 0.26,
+                  ),
                   Text(
-                    "My pages",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  Spacer(),
-                  Padding(
-                    padding: EdgeInsets.only(top: 5),
-                    child: Text(
-                      "See all",
-                      style: TextStyle(fontSize: 10, color: Colors.black54),
-                    ),
-                  ),
+                    getTranslated('Pages', context)
+                    ,
+                    style: latoStyle500Medium,
+                  )
                 ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 12, right: 12),
-              child: SizedBox(
-                height: height * 0.26,
-                child: GridView.builder(
-                    gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                      maxCrossAxisExtent: 150,
-                      childAspectRatio: 2.7,
-                      crossAxisSpacing: 10,
+              SizedBox(
+                height: height * 0.03,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: height * 0.035,
+                    width: width * 0.24,
+                    decoration: BoxDecoration(
+                        color: const Color(0xff090D2A), borderRadius: BorderRadius.circular(4)),
+                    child: Center(
+                        child: Text( getTranslated("My page", context),
+                      style: latoStyle200ExtraLight.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    )),
+                  ),
+                  SizedBox(
+                    width: width * 0.03,
+                  ),
+                  Container(
+                    height: height * 0.035,
+                    width: width * 0.24,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black, width: 1)),
+                    child: Center(
+                        child: Text(getTranslated("Discover", context),
+                      style: latoStyle200ExtraLight.copyWith(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                    )),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: height * 0.03,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Row(
+                  children: [
+                    Text(getTranslated("My pages", context), style: latoStyle700Bold),
+                    Spacer(),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5),
+                      child: Text("See all", style: latoStyle100Thin.copyWith(fontSize: 10)),
                     ),
-                    itemCount: 6,
-                    itemBuilder: (BuildContext ctx, index) {
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const CircleAvatar(
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 12, right: 12),
+                child: SizedBox(
+                  height: height * 0.26,
+                  child: pageProvider.authorPageLists.isEmpty
+                      ? Center(child: Text(getTranslated("Sorry you don't have any page", context),style: latoStyle700Bold,))
+                      : GridView.builder(
+                          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 150,
+                            childAspectRatio: 2.7,
+                            crossAxisSpacing: 10,
+                          ),
+                          itemCount: pageProvider.authorPageLists.length>5?pageProvider.authorPageLists.length:5,
+                          itemBuilder: (BuildContext ctx, index) {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CircleAvatar(
+                                  radius: 24,
+                                  backgroundColor: Colors.black12,
+                                  backgroundImage:
+                                      NetworkImage(pageProvider.authorPageLists[index].avatar),
+                                ),
+                                SizedBox(
+                                  width: width * 0.01,
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      height: height * 0.01,
+                                    ),
+                                    Text(pageProvider.authorPageLists[index].name,
+                                        style: latoStyle700Bold),
+                                    const SizedBox(
+                                      height: 2,
+                                    ),
+                                    Text(
+                                        "${pageProvider.authorPageLists[index].followers.toString()} ${getTranslated("Followers", context)}",
+                                        style: latoStyle100Thin.copyWith(fontSize: 10)),
+                                  ],
+                                )
+                              ],
+                            );
+                          }),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Row(
+                  children: [
+                    Text(getTranslated("Page you liked", context) ,style: latoStyle700Bold),
+                    const Spacer(),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5),
+                      child: Text(getTranslated("See all", context), style: latoStyle100Thin.copyWith(fontSize: 10)),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: height * 0.26,
+                child: pageProvider.allSuggestPageList.isEmpty?CupertinoActivityIndicator():ListView.builder(
+                    itemCount: pageProvider.allSuggestPageList.length>2?pageProvider.allSuggestPageList.length:2,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                          leading:  CircleAvatar(
                             radius: 24,
                             backgroundColor: Colors.black12,
+                            backgroundImage: NetworkImage(pageProvider.allSuggestPageList[index].avatar),
                           ),
-                          SizedBox(
-                            width: width * 0.01,
+                          trailing: Container(
+                            height: height * 0.027,
+                            width: width * 0.15,
+                            decoration: BoxDecoration(
+                                color: const Color(0xff090D2A),
+                                borderRadius: BorderRadius.circular(4)),
+                            child: Center(
+                                child: Text(
+                                  getTranslated("Follow", context),
+                                  style:
+                                  latoStyle600SemiBold.copyWith(color: Colors.white, fontSize: 10),
+                                )),
                           ),
-                          Column(
+                          title: Column(
                             mainAxisAlignment: MainAxisAlignment.start,
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               SizedBox(
                                 height: height * 0.01,
                               ),
-                              const Text(
-                                "Page name",
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                              ),
+                              Text(pageProvider.allSuggestPageList[index].name, style: latoStyle700Bold),
                               const SizedBox(
                                 height: 2,
                               ),
-                              const Text(
-                                "5908 likes",
-                                style: TextStyle(color: Colors.black26, fontSize: 10),
-                              ),
+                              Text("${pageProvider.allSuggestPageList[index].followers.toString()} ${getTranslated("Followers", context)}", style: latoStyle100Thin.copyWith(fontSize: 10)),
                             ],
-                          )
-                        ],
-                      );
+                          ));
                     }),
               ),
-            ),
-            SizedBox(
-              height: height * 0.02,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10, right: 10),
-              child: Row(
-                children: const [
-                  Text(
-                    "Page you liked",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  Spacer(),
-                  Padding(
-                    padding: EdgeInsets.only(top: 5),
-                    child: Text(
-                      "See all",
-                      style: TextStyle(fontSize: 10, color: Colors.black54),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: height * 0.26,
-              child: ListView.builder(
-                  itemCount: 3,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                        leading: const CircleAvatar(
-                          radius: 24,
-                          backgroundColor: Colors.black12,
-                        ),
-                        trailing: Container(
-                          height: height*0.027,
-                          width: width*0.15,
-                          decoration: BoxDecoration(
-                              color: const Color(0xff090D2A),
-                              borderRadius: BorderRadius.circular(4)
-                          ),
-                          child: const Center(child: Text("Follow",style: TextStyle(color:Colors.white),)),
-                        ),
-                        title: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              height: height * 0.01,
-                            ),
-                            const Text(
-                              "Page name",
-                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                            ),
-                            const SizedBox(
-                              height: 2,
-                            ),
-                            const Text(
-                              "5908 likes",
-                              style: TextStyle(color: Colors.black26, fontSize: 10),
-                            ),
-                          ],
-                        ));
-                  }),
-            ),
-          ],
-        ),
+            ],
+          );
+        }),
       ),
     );
   }
