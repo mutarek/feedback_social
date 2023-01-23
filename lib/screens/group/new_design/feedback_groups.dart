8df0e4cd98803eff45633b12c3d726d63bbc59b5
macbookair <faysaltanvir9991@gmail.com>
2023-01-23 14:45:03 +0600

    your feed page

--- lib/screens/group/new_design/feedback_groups.dart
+++ lib/screens/group/new_design/feedback_groups.dart
@@ -1,5 +1,6 @@
 
 import 'package:als_frontend/provider/group_provider.dart';
+import 'package:als_frontend/screens/group/new_design/your_feed_group_screen.dart';
 import 'package:als_frontend/screens/group/widget/group_view_card.dart';
 import 'package:als_frontend/screens/page/find_page.dart';
 import 'package:als_frontend/screens/page/page_dashboard.dart';
@@ -85,30 +86,33 @@ class FeedBackGroups extends StatelessWidget {
             child: Column(
               children: [
                 const SizedBox(height: 10),
-                Container(
-                  decoration: BoxDecoration(color: const Color(0xffF0F2F5), borderRadius: BorderRadius.circular(30)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        CircleAvatar(
-                            radius: 20, backgroundColor: Colors.black, child: SvgPicture.asset("assets/svg/your_feed_icon.svg",height: 22,width: 25,)),
-                        const SizedBox(width: 12),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Your Feed", style: robotoStyle700Bold.copyWith(fontSize: 20)),
-                              const SizedBox(height: 2),
-                              Text("View the most recent activity of the groups you follow.",
-                                  style: robotoStyle400Regular.copyWith(fontSize: 10))
-                            ],
+                InkWell(
+                  onTap: (){Helper.toScreen(YourFeedGroupScreen(dicription: '', showDescription: () {  }, value: false,));},
+                  child: Container(
+                    decoration: BoxDecoration(color: const Color(0xffF0F2F5), borderRadius: BorderRadius.circular(30)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CircleAvatar(
+                              radius: 20, backgroundColor: Colors.black, child: SvgPicture.asset("assets/svg/your_feed_icon.svg",height: 22,width: 25,)),
+                          const SizedBox(width: 12),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Your Feed", style: robotoStyle700Bold.copyWith(fontSize: 20)),
+                                const SizedBox(height: 2),
+                                Text("View the most recent activity of the groups you follow.",
+                                    style: robotoStyle400Regular.copyWith(fontSize: 10))
+                              ],
+                            ),
                           ),
-                        ),
-                        const SizedBox(width: 10),
-                      ],
+                          const SizedBox(width: 10),
+                        ],
+                      ),
                     ),
                   ),
                 ),
