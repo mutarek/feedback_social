2f8cd0af486102f1fba94f277746f6beafbb6cbf
Mehedi Hasan Shuvo <67133203+duetianmehedishuvo@users.noreply.github.com>
2023-02-09 17:18:20 +0600

    Merge with tarek and faysal

--- lib/screens/group/widget/group_media_widget.dart
+++ lib/screens/group/widget/group_media_widget.dart
@@ -1,4 +1,5 @@
 import 'package:als_frontend/provider/group_provider.dart';
+import 'package:als_frontend/screens/page/photo_video_view_screen.dart';
 import 'package:als_frontend/util/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
@@ -12,7 +13,6 @@ import '../../../util/image.dart';
 import '../../../util/theme/text.styles.dart';
 import '../../../widgets/network_image.dart';
 import '../../../widgets/single_image_view.dart';
-import '../../page/new_design/photo_video_view_screen.dart';
 
 class GroupMediaWidget extends StatefulWidget {
   final AuthorEachGroupModel authorEachGroupModel;
